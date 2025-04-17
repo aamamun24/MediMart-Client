57c1869f6c0c9bba5ec0a4fb4c8c3d282956543f
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-04-17 23:54:50 +0600

    This medimart in create footer successfully from Antor Karmokar

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,98 @@
+import Link from "next/link";
+import {
+  Facebook,
+  Instagram,
+  Mail,
+  MapPinned,
+  Phone,
+  Twitter,
+} from "lucide-react";
+import Logo from "./Logo";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#09192c] border-b-2 border-white py-6">
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {/* logo inner */}
+          <div>
+            {/* logo */}
+            <Logo />
+            {/* description */}
+            <p className="text-white text-sm mt-6">
+              Medimart is your trusted online medicine and healthcare shop,
+              offering a wide range of genuine medicines, health products, and
+              wellness essentials — delivered safely to your doorstep.
+            </p>
+          </div>
+          {/* quick link inner */}
+          <div>
+            <h3 className="text-white text-3xl font-semibold mb-6 ">
+              Quick Links
+            </h3>
+            <ul className="text-white leading-8">
+              <li className="hover:underline transition ease-in-out">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="hover:underline transition ease-in-out">
+                <Link href="/shop">Shop</Link>
+              </li>
+              <li className="hover:underline transition ease-in-out">
+                <Link href="/cart">Cart</Link>
+              </li>
+              <li className="hover:underline">
+                <Link href="/checkout">Checkout</Link>
+              </li>
+              <li className="hover:underline transition ease-in-out">
+                <Link href="/orders">Orders</Link>
+              </li>
+            </ul>
+          </div>
+          {/* contact inner and social inner */}
+          <div>
+            <h3 className="text-white font-semibold text-3xl mb-6">
+              Contact Us
+            </h3>
+            <ul className="text-white leading-8">
+              <li className="flex items-center gap-4 mb-4">
+                <Phone />
+                <span>+1 (415) 555-0198</span>
+              </li>
+              <li className="flex items-center gap-4 mb-4">
+                <Mail />
+                <span>support@medimart.com</span>
+              </li>
+              <li className="flex items-center gap-4 mb-4">
+                <MapPinned />
+                <span>
+                  Medimart Support Center 742 Evergreen Terrace Springfield, IL
+                  62704 United States
+                </span>
+              </li>
+              <li className="flex gap-4">
+                <Link href="https://www.facebook.com/">
+                  <Facebook />
+                </Link>
+                <Link href="https://www.instagram.com/">
+                  <Instagram />
+                </Link>
+                <Link href="https://x.com/?lang=en">
+                  <Twitter />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr className="border-t-2 border-white"/>
+      {/* All rights reserved */}
+      <div className="mt-6">
+        <p className="text-white text-center">
+          &copy; 2025 Medimart. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
