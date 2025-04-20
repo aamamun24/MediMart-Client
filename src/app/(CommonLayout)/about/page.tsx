1e2df8f67050047187ed0663065c18169091be5f
Jakya Afrin Bristi <jakyaafrinb@gmail.com>
2025-04-20 12:40:21 +0600

    about-us

---
+++ src/app/(CommonLayout)/about/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+import Image from 'next/image';
+import React from 'react'
+
+export default function AboutUs() {
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-2 container mx-auto mt-10 gap-8 px-5 mb-28">
+          
+          
+          <div className="relative flex justify-center lg:justify-end">
+        
+            <div className="relative w-full lg:w-3/4 h-[400px]">
+              <Image
+                src="https://plus.unsplash.com/premium_photo-1668487826666-baa00865bc13?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="About Image 1"
+                fill
+                className="rounded-tr-2xl rounded-bl-2xl object-cover"
+                sizes="(max-width: 1024px) 100vw, 75vw"
+              />
+            </div>
+            
+          
+            <div className="w-[60vw] sm:w-[40vw] md:w-[30vw] lg:w-[25vw] h-[250px] absolute left-5 -bottom-20 hidden xl:block">
+              <Image
+                src="https://images.unsplash.com/photo-1527613426441-4da17471b66d?q=80&w=1452&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="About Image 2"
+                fill
+                className="rounded-tr-2xl rounded-bl-2xl border-t-8 border-r-8 border-t-white border-r-white shadow-lg object-cover"
+                sizes="(max-width: 1536px) 25vw, 384px"
+              />
+            </div>
+          </div>
+    
+          <div className="flex flex-col justify-center text-center lg:text-left">
+            <h1 className="font-bold text-2xl underline mb-5 text-center">About MediMart</h1>
+            <p className="max-w-3xl mx-auto lg:mx-0 px-6 font-semibold text-gray-500">
+              At MediMart, we are dedicated to providing safe, reliable, and affordable 
+              medications to everyone. Our mission is to make healthcare accessible 
+              through our trusted online pharmacy platform.
+            </p>
+          </div>
+        </div>
+    );
+}
--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import Review from "@/components/review/Review";
+import AboutUs from "./about/page";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="px-4 py-8">
       <h1>Welcome to MediMart</h1>
+      <AboutUs></AboutUs>
       <Review />
     </div>
   );
